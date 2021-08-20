189e49aa6aab478ca7a5ed7d80d606ec1d3d3993
Jacob-Short <thatmanjshort@gmail.com>
2021-08-19 21:09:46 -0700

    added articles along with bootstrap,css

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [
+    {
+        'author': 'Jacob Short',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 19, 2021'
+    },
+        {
+        'author': 'Raelynn Short',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 19, 2021'
+    },
+        {
+        'author': 'Samantha Short',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'August 19, 2021'
+    },
+
+
+]
+
 def home_view(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about_view(request):
-    return HttpResponse('<h1>Blog About</h1>')
+        return render(request, 'blog/about.html', {'title': 'About'})
+
